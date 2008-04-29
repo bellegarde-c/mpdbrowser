50dda66bb0bf9ce90aa50ce2d2b15582e39ee960
Bellegarde Cedric <gnumdk@flanders.crid-85.in.ac-nantes.fr>
2008-04-29 10:05:47 +0200

    Gtk progress bar is slow :(

--- src/mpdBrowserDatabase.py
+++ src/mpdBrowserDatabase.py
@@ -67,15 +67,17 @@ class mpdBrowserDatabase (threading.Thread, IdleObject):
             albums = self.__conn.list ('album')
             
             totalAlbums = len (albums)
-            nbAlbums = 0
+            nbAlbums = 0 # Will just represent albums with != names
             for album in albums:
                 if self.__stopevent.isSet():
                     return
                 # Get album genre, artist, cover and path
                 tmpAlbums = self.__getAlbumInfos (album)
                 albumList += tmpAlbums
-                nbAlbums += len (tmpAlbums)
-                self.emit ("progress", float (nbAlbums) / float (totalAlbums))
+                nbAlbums += 1
+                if not nbAlbums % 50: # Speed gain
+                    self.emit ("progress", 
+                               float (nbAlbums) / float (totalAlbums))
 
             self.__conn.close ()
             self.emit ("scanned", albumList)
@@ -111,30 +113,30 @@ class mpdBrowserDatabase (threading.Thread, IdleObject):
         infos = self.__conn.find ('album', album)
 
         for i in range (len (infos)):
-            # Deal with missing tags
-            if 'artist' not in infos[i].keys ():
-                infos[i]['artist'] = _("Unknown")
-            if 'album' not in infos[i].keys():
-                infos[i]['album'] = _("Unknown")
-            if 'genre' not in infos[i].keys():
-                infos[i]['genre'] = _("Unknown")
-                
-            # Deal with multiple tags
-            # Force an Attribute Error to be raise
-            try:
-                infos[i]['artist'].sort ()
-                artist = infos[i]['artist'][0]
-            except:
-                artist = infos[i]['artist']
-            try:
-                infos[i]['genre'].sort ()
-                genre = infos[i]['genre'][0]
-            except:
-                genre = infos[i]['genre']
-                    
             path = os.path.dirname ( self.__path + infos[i]['file'])
             if currentPath != path: # We can have two albums with same name
                 try:
+                    # Deal with missing tags
+                    if 'artist' not in infos[i].keys ():
+                        infos[i]['artist'] = _("Unknown")
+                    if 'album' not in infos[i].keys():
+                        infos[i]['album'] = _("Unknown") # Is that possible?
+                    if 'genre' not in infos[i].keys():
+                        infos[i]['genre'] = _("Unknown")
+                
+                    # Deal with multiple tags
+                    # Force an Attribute Error to be raise
+                    try:
+                        infos[i]['artist'].sort ()
+                        artist = infos[i]['artist'][0]
+                    except:
+                        artist = infos[i]['artist']
+                    try:
+                        infos[i]['genre'].sort ()
+                        genre = infos[i]['genre'][0]
+                    except:
+                        genre = infos[i]['genre']
+                    
                     pixbuf = self.__covers.get (path)
                     currentPath = path
                     list.append ((genre, artist, album, path, pixbuf))
